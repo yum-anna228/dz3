d3d485a1c9f1d3266a61ccdf2c2d450cea0c7d39
anna <anna@ANYA>
2024-11-21 22:41:57 +0300

    Добавьте файлы проекта.

---
+++ dzz/Program.cs
@@ -0,0 +1,164 @@
+﻿namespace dzz
+{
+    internal class Program
+    {
+        enum Dayweek
+        {
+            Понедельник = 1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Дана последовательность из 10 чисел.Определить, является ли эта последовательность
+            //упорядоченной по возрастанию.В случае отрицательного ответа определить
+            //порядковый номер первого числа, которое нарушает данную последовательность.
+            //Использовать break.
+            Console.WriteLine("Задание 1");
+            int[] numbers = new int[10];
+            Console.WriteLine("Введите 10 чисел:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Число {i+1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            bool isOrdered = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    Console.WriteLine($"Последовательность не упорядочена. Порядковый номер первого числа, которое нарушает данную последовательность: {i+2}");
+                    isOrdered = false;
+                    break;
+                }
+            }
+            if (isOrdered)
+            {
+                Console.WriteLine("Последовательность упорядочена по возрастанию.");
+            }
+            
+
+            //Игральным картам условно присвоены следующие порядковые номера в зависимости от
+            //их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.
+            //Порядковые номера остальных карт соответствуют их названиям(«шестерка»,
+            //«девятка» и т.п.).По заданному номеру карты k(6 <= k <= 14) определить достоинство
+            //соответствующей карты. Использовать try-catch-finally.
+            Console.WriteLine("Задание 2");
+            try
+            {
+                Console.Write("Введите порядковый номер карты (6 - 14): ");
+                int k = int.Parse(Console.ReadLine());
+                if (k < 6 || k > 14)
+                {
+                    Console.WriteLine("Неверно введён порядковый номер карты.");
+                }
+                else
+                {
+                    string cardValue;
+                    switch(k)
+                    {
+                        case 6: cardValue = "Шестёрка"; break;
+                        case 7: cardValue = "Семёрка"; break;
+                        case 8: cardValue = "Восьмёрка"; break;
+                        case 9: cardValue = "Девятка"; break;
+                        case 10: cardValue = "Десятка"; break;
+                        case 11: cardValue = "Валет"; break;
+                        case 12: cardValue = "Дама"; break;
+                        case 13: cardValue = "Король"; break;
+                        case 14: cardValue = "Туз"; break;
+                        default: cardValue = "Неизвестная карта"; break;
+                    };
+
+                    Console.WriteLine($"Достоинство карты: {cardValue}");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите целое число.");
+            }
+            finally
+            {
+                Console.WriteLine("Работа завершена.");
+            }
+
+            //Написать программу, которая принимает на входе строку и производит выходные
+            //данные в соответствии с таблицей.
+            Console.WriteLine("Задание 3");
+            Console.WriteLine("Введите кем вы являетесь: ");
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+
+                    //Составить программу, которая в зависимости от порядкового номера дня недели(1, 2,
+                    //..., 7) выводит на экран его название(понедельник, вторник, ..., воскресенье).
+                    //Использовать enum.
+                    Console.WriteLine("Задание 4");
+            try
+            {
+                Console.Write("Введите порядковый номер дня недели (1 - 7): ");
+                int dayNumber = int.Parse(Console.ReadLine());
+                if (dayNumber < 1 || dayNumber > 7)
+                {
+                    Console.WriteLine("Номер дня недели должен быть в диапазоне от 1 до 7");
+                }
+                else
+                {
+                    Dayweek day = (Dayweek)dayNumber;
+                    Console.WriteLine($"День недели: {day}");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите целое число.");
+            }
+            finally
+            {
+                Console.WriteLine("Работа завершена");
+            }
+
+            //Создать массив строк.При помощи foreach обойти весь массив.При встрече элемента
+            //"Hello Kitty" или "Barbie doll" необходимо положить их в “сумку”, т.е.прибавить к
+            //результату.Вывести на экран сколько кукол в “сумке”.
+            Console.WriteLine("Задание 5");
+            string[] toys = { "Hello Kitty", "Lego", "Barbie doll", "Ball", "Board game", "Hello Kitty", "Dolls house", "Barbie Doll" };
+            int bag = 0;
+            foreach (string toy in toys)
+            {
+                if (toy == "Hello Kitty" || toy == "Barbie doll")
+                    bag++;
+            }
+            Console.WriteLine($"Количество кукол в сумке равно {bag}");
+
+
+        }
+    }
+}
---
+++ tumakov/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace tumakov
+{
+    internal class Program
+    {
+        static string MonthName(int month)
+        {
+            switch (month)
+            {
+                case 1: return "января";
+                case 2: return "февраля";
+                case 3: return "марта";
+                case 4: return "апреля";
+                case 5: return "мая";
+                case 6: return "июня";
+                case 7: return "июля";
+                case 8: return "августа";
+                case 9: return "сентября";
+                case 10: return "октября";
+                case 11: return "ноября";
+                case 12: return "декабря";
+                default: return "";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 4.1");
+            int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            Console.Write("Введите номер дня в году (от 1 до 365): ");
+            if (int.TryParse(Console.ReadLine(), out int dayOfYear))
+            {
+                int month = 0;
+                int day = dayOfYear;
+                for (int i = 0; i < daysInMonth.Length; i++)
+                {
+                    if (day <= daysInMonth[i])
+                    {
+                        month = i + 1; 
+                        break;
+                    }
+                    else
+                    {
+                        day -= daysInMonth[i]; 
+                    }
+                }
+                Console.WriteLine($"{dayOfYear} соответствует {day} {MonthName(month)}.");
+            }
+            
+
+            Console.WriteLine("Задание 4.2");
+            int[] daysInMonth2 = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            Console.Write("Введите номер дня в году (от 1 до 365): ");
+            if (int.TryParse(Console.ReadLine(), out int dayOfYear2) && dayOfYear2 >= 1 && dayOfYear2 <= 365)
+            {
+                int month = 0;
+                int day = dayOfYear2;
+                for (int i = 0; i < daysInMonth2.Length; i++)
+                {
+                    if (day <= daysInMonth2[i])
+                    {
+                        month = i + 1; 
+                        break;
+                    }
+                    else
+                    {
+                        day -= daysInMonth2[i]; 
+                    }
+                }
+                Console.WriteLine($"{dayOfYear2} соответствует {day} {MonthName(month)}.");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введите корректное число от 1 до 365.");
+            }
+
+            Console.WriteLine("Задание 4.3");
+                Console.Write("Введите год: ");
+                if (!int.TryParse(Console.ReadLine(), out int year))
+                {
+                    throw new ArgumentException("Ошибка: Введите корректный год.");
+                }
+                bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+                if (isLeapYear)
+                {
+                    Console.WriteLine($"{year} является високосным годом.");
+                }
+                else
+                {
+                    Console.WriteLine($"{year} не является високосным годом.");
+                }
+                int maxDays = isLeapYear ? 366 : 365;
+                Console.Write($"Введите номер дня в году (от 1 до {maxDays}): ");
+
+                if (!int.TryParse(Console.ReadLine(), out int dayOfYear3))
+                {
+                    throw new ArgumentException("Ошибка: Введите корректное число.");
+                }
+                if (dayOfYear3 < 1 || dayOfYear3 > maxDays)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(dayOfYear3), "Ошибка: Введите число от 1 до " + maxDays + ".");
+                }
+                int[] daysInMonth3 = { 31, isLeapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+                int month3 = 0;
+                int day3 = dayOfYear3;
+                for (int i = 0; i < daysInMonth3.Length; i++)
+                {
+                    if (day3 <= daysInMonth3[i])
+                    {
+                        month3 = i + 1; 
+                        break;
+                    }
+                    else
+                    {
+                        day3 -= daysInMonth3[i]; 
+                    }
+                }
+                Console.WriteLine($"{dayOfYear3} соответствует {day3} {MonthName(month3)} {year} года.");
+            
+
+
+        }
+    }
+}
